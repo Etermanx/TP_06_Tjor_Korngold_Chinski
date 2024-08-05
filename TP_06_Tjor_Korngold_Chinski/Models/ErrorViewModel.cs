f135bb168fedf33d01d0c83ec6a95a00a8c6cf87
Tomás Korngold y Lucas Tjor <tklt@localhost>
2024-08-05 10:21:53 -0300

    Resubir datos iniciales de proyecto + clases Deportista, Pais y Deporte

---
+++ TP_06_Tjor_Korngold_Chinski/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP_06_Tjor_Korngold_Chinski.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
