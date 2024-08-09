10a00740b20d178087a644dfa0bf706e09da228d
Lucas Tjor <tjor75@gmail.com>
2024-08-09 15:48:21 -0300

    Crear action Paises

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,6 +17,11 @@ public class HomeController : Controller
     {
         return View();
     }
+    public IActionResult Paises()
+    {
+        ViewBag.Paises = BD.ListarPaises();
+        return View();
+    }
     public IActionResult Deportes()
     {
         ViewBag.Deportes = BD.ListarDeportes();
