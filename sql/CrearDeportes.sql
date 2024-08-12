12b62a834806df5b398898e077151f55af73b5b0
Lucas Tjor <tjor75@gmail.com>
2024-08-12 08:03:46 -0300

    Crear SQLs para probar paises y deportes

---
+++ sql/CrearDeportes.sql
@@ -0,0 +1,8 @@
+USE JJOO;
+
+INSERT INTO Deportes (Nombre, Foto)
+VALUES
+    ('Básquet', 'https://olympics.com/images/static/sports/pictograms/v2/bkb.svg'),
+    ('Fútbol', 'https://olympics.com/images/static/sports/pictograms/v2/fbl.svg'),
+    ('Gimnasia artística', 'https://olympics.com/images/static/sports/pictograms/v2/gac.svg'),
+    ('Hockey', 'https://olympics.com/images/static/sports/pictograms/v2/hoc.svg');
---
+++ sql/CrearPaises.sql
@@ -0,0 +1,9 @@
+USE JJOO;
+
+INSERT INTO Paises (Nombre, Bandera, FechaFundacion)
+VALUES
+    ('Afganistán', 'https://gstatic.olympics.com/s1/f_auto/t_original/static/ioc/flag/3x2/AFG', '1936-01-01'),
+    ('Argentina', 'https://gstatic.olympics.com/s1/f_auto/t_original/static/ioc/flag/3x2/ARG', '1952-01-01'),
+    ('China', 'https://gstatic.olympics.com/s1/f_auto/t_original/static/ioc/flag/3x2/CHN', '1889-01-01'),
+    ('Estados Unidos', 'https://gstatic.olympics.com/s1/f_auto/t_original/static/ioc/flag/3x2/USA', '1889-01-01'),
+    ('Italia', 'https://gstatic.olympics.com/s1/f_auto/t_original/static/ioc/flag/3x2/ITA', '1900-01-01');
