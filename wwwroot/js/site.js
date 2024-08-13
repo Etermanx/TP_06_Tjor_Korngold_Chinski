45517e7e9e0352522d2243e415a3b89aab2025f2
Lucas Tjor <tjor75@gmail.com>
2024-08-13 13:12:06 -0300

    Trabajar en sticky Header. WIP

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,21 +1,33 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(() => {
-    'use strict'
+// Modificado de https://getbootstrap.com/docs/5.3/forms/validation/
+function configurarValidacionFormsBoostrap() {
+  const forms = document.querySelectorAll('.needs-validation')
+
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+    });
+  });
+
+  form.classList.add("was-validated");
+}
+
+function cambiarHeaderSticky() {
+  const STICKY_CLASS = "sticky";
+  const ALTURA_HEADER = 60;
+  const navElement = document.getElementsByTagName("nav")[0];
+  let distanciaStickyConseguida = this.scrollY > ALTURA_HEADER;
+  let stickyActivo = navElement.classList.contains(STICKY_CLASS);
   
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-  
-        form.classList.add('was-validated')
-      }, false)
-    })
-  })()
-  
-  
+
+  if (distanciaStickyConseguida && !stickyActivo)
+    navElement.classList.add(STICKY_CLASS);
+  else if (!distanciaStickyConseguida && stickyActivo)
+    navElement.classList.remove(STICKY_CLASS);
+}
+
+
+configurarValidacionFormsBoostrap();
+window.addEventListener("scroll", cambiarHeaderSticky, false);
