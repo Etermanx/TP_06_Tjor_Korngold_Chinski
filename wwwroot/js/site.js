df4df32c6c2446164a10f74b4a569ea64fcc40f9
Lucas Tjor <tjor75@gmail.com>
2024-08-13 21:09:58 -0300

    Arreglar sticky del navbar

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,31 +1,31 @@
 // Modificado de https://getbootstrap.com/docs/5.3/forms/validation/
 function configurarValidacionFormsBoostrap() {
-  const forms = document.querySelectorAll('.needs-validation')
+  const forms = document.querySelectorAll('.needs-validation');
 
   Array.from(forms).forEach(form => {
     form.addEventListener('submit', event => {
       if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
+        event.preventDefault();
+        event.stopPropagation();
       }
     });
-  });
 
-  form.classList.add("was-validated");
+    form.classList.add("was-validated");
+  });
 }
 
 function cambiarHeaderSticky() {
   const STICKY_CLASS = "sticky";
   const ALTURA_HEADER = 60;
-  const navElement = document.getElementsByTagName("nav")[0];
-  let distanciaStickyConseguida = this.scrollY > ALTURA_HEADER;
-  let stickyActivo = navElement.classList.contains(STICKY_CLASS);
+  const headerElement = document.getElementsByTagName("header")[0];
+  let distanciaStickyConseguida = this.scrollY >= ALTURA_HEADER;
+  let stickyActivo = headerElement.classList.contains(STICKY_CLASS);
   
 
   if (distanciaStickyConseguida && !stickyActivo)
-    navElement.classList.add(STICKY_CLASS);
+    headerElement.classList.add(STICKY_CLASS);
   else if (!distanciaStickyConseguida && stickyActivo)
-    navElement.classList.remove(STICKY_CLASS);
+    headerElement.classList.remove(STICKY_CLASS);
 }
 
 
