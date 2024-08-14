f8b76e1dcd31a03987d0f00f76b23a5e3fea390f
Lucas Tjor <tjor75@gmail.com>
2024-08-14 09:03:46 -0300

    Hacer la view PoliticaPrivacidad

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -124,4 +124,9 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+    public IActionResult PoliticaPrivacidad()
+    {
+        return View();
+    }
+    
 }
