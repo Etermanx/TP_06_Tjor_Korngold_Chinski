6027c7d48e78e7d1ace646dba5d0f4a6ef85ecd1
Lucas Tjor <tjor75@gmail.com>
2024-08-16 14:47:02 -0300

    Cambiar Datos en BD

--- sql/CrearPaises.sql
+++ sql/CrearPaises.sql
@@ -2,8 +2,8 @@ USE JJOO;
 
 INSERT INTO Paises (Nombre, Bandera, FechaFundacion)
 VALUES
-    ('Afganistán', 'https://gstatic.olympics.com/s1/f_auto/t_original/static/ioc/flag/3x2/AFG', '1936-01-01'),
-    ('Argentina', 'https://gstatic.olympics.com/s1/f_auto/t_original/static/ioc/flag/3x2/ARG', '1952-01-01'),
+    ('Afganistán', 'https://gstatic.olympics.com/s1/f_auto/t_original/static/ioc/flag/3x2/AFG', '1936-08-01'),
+    ('Argentina', 'https://gstatic.olympics.com/s1/f_auto/t_original/static/ioc/flag/3x2/ARG', '1952-07-05'),
     ('China', 'https://gstatic.olympics.com/s1/f_auto/t_original/static/ioc/flag/3x2/CHN', '1889-01-01'),
     ('Estados Unidos', 'https://gstatic.olympics.com/s1/f_auto/t_original/static/ioc/flag/3x2/USA', '1889-01-01'),
     ('Italia', 'https://gstatic.olympics.com/s1/f_auto/t_original/static/ioc/flag/3x2/ITA', '1900-01-01');
