f23e3388545de53a84c44546c5247a24963b948c
Lucas Tjor <tjor75@gmail.com>
2024-08-16 15:45:34 -0300

    Cambios

--- sql/CrearPaises.sql
+++ sql/CrearPaises.sql
@@ -3,7 +3,7 @@ USE JJOO;
 INSERT INTO Paises (Nombre, Bandera, FechaFundacion)
 VALUES
     ('Afganistán', 'https://gstatic.olympics.com/s1/f_auto/t_original/static/ioc/flag/3x2/AFG', '1936-08-01'),
-    ('Argentina', 'https://gstatic.olympics.com/s1/f_auto/t_original/static/ioc/flag/3x2/ARG', '1952-07-05'),
-    ('China', 'https://gstatic.olympics.com/s1/f_auto/t_original/static/ioc/flag/3x2/CHN', '1889-01-01'),
-    ('Estados Unidos', 'https://gstatic.olympics.com/s1/f_auto/t_original/static/ioc/flag/3x2/USA', '1889-01-01'),
-    ('Italia', 'https://gstatic.olympics.com/s1/f_auto/t_original/static/ioc/flag/3x2/ITA', '1900-01-01');
+    ('Argentina', 'https://gstatic.olympics.com/s1/f_auto/t_original/static/ioc/flag/3x2/ARG', '1924-07-05'),
+    ('China', 'https://gstatic.olympics.com/s1/f_auto/t_original/static/ioc/flag/3x2/CHN', '1924-07-05'),
+    ('Estados Unidos', 'https://gstatic.olympics.com/s1/f_auto/t_original/static/ioc/flag/3x2/USA', '1896-04-06'),
+    ('Italia', 'https://gstatic.olympics.com/s1/f_auto/t_original/static/ioc/flag/3x2/ITA', '1896-04-06');
