bb05ed92900eeba221cf19da7d8edc7efdfe940c
Lucas Tjor <tjor75@gmail.com>
2024-08-19 08:28:57 -0300

    Agregar banner a deportistas

--- sql/CrearDeportes.sql
+++ sql/CrearDeportes.sql
@@ -1,8 +1,8 @@
 USE JJOO;
 
-INSERT INTO Deportes (Nombre, Foto)
+INSERT INTO Deportes (Nombre, Foto, Banner)
 VALUES
     ('Básquet', 'https://olympics.com/images/static/sports/pictograms/v2/bkb.svg', 'https://img.olympicchannel.com/images/image/private/t_16-9_1920/f_auto/primary/jnefp96qvymodu9lflih'),
-    ('Fútbol', 'https://olympics.com/images/static/sports/pictograms/v2/fbl.svg'),
-    ('Gimnasia artística', 'https://olympics.com/images/static/sports/pictograms/v2/gac.svg'),
-    ('Hockey', 'https://olympics.com/images/static/sports/pictograms/v2/hoc.svg');
+    ('Fútbol', 'https://olympics.com/images/static/sports/pictograms/v2/fbl.svg', 'https://img.olympicchannel.com/images/image/private/t_16-9_1920/f_auto/primary/baa2nr7jo3wx6wv20vww'),
+    ('Gimnasia artística', 'https://olympics.com/images/static/sports/pictograms/v2/gac.svg', 'https://img.olympicchannel.com/images/image/private/t_16-9_1920/f_auto/primary/kurzlxstqem0gndjsdg6'),
+    ('Hockey', 'https://olympics.com/images/static/sports/pictograms/v2/hoc.svg', 'https://img.olympicchannel.com/images/image/private/t_16-9_1920/f_auto/primary/tm9btf3cbz1rzcosp8zq');
