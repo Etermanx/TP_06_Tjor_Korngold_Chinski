4ee284227eec4ba9afd93cb9f8cf5d4da3398010
Lucas Tjor <tjor75@gmail.com>
2024-08-19 08:27:28 -0300

    Cambiar modelo para mostrar deportistas por uno más correcto en HTML

--- Models/Deporte.cs
+++ Models/Deporte.cs
@@ -2,4 +2,5 @@ public class Deporte {
     public int IdDeporte { get; set; }
     public string Nombre { get; set; }
     public string Foto { get; set; }
+    public string Banner { get; set; }
 }
--- sql/CrearDeportes.sql
+++ sql/CrearDeportes.sql
@@ -2,7 +2,7 @@ USE JJOO;
 
 INSERT INTO Deportes (Nombre, Foto)
 VALUES
-    ('Básquet', 'https://olympics.com/images/static/sports/pictograms/v2/bkb.svg'),
+    ('Básquet', 'https://olympics.com/images/static/sports/pictograms/v2/bkb.svg', 'https://img.olympicchannel.com/images/image/private/t_16-9_1920/f_auto/primary/jnefp96qvymodu9lflih'),
     ('Fútbol', 'https://olympics.com/images/static/sports/pictograms/v2/fbl.svg'),
     ('Gimnasia artística', 'https://olympics.com/images/static/sports/pictograms/v2/gac.svg'),
     ('Hockey', 'https://olympics.com/images/static/sports/pictograms/v2/hoc.svg');
