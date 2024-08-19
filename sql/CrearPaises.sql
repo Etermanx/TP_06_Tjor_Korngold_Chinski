14888588950ed4c05cf33cc2fe40438b60f7d749
Lucas Tjor <tjor75@gmail.com>
2024-08-19 19:00:03 -0300

    Crear botón para eliminar deportista y corregir errores en ejemplos SQL

--- sql/CrearPaises.sql
+++ sql/CrearPaises.sql
@@ -3,8 +3,8 @@ USE JJOO;
 INSERT INTO Paises (Nombre, Bandera, FechaFundacion,Himno)
 VALUES
     ('Afganistán', 'https://gstatic.olympics.com/s1/f_auto/t_original/static/ioc/flag/3x2/AFG', '1936-08-01','https://upload.wikimedia.org/wikipedia/commons/6/6f/National_anthem_of_Afghanistan%2C_performed_by_the_United_States_Navy_Band.wav'),
-    ('Albania', 'https://gstatic.olympics.com/s1/f_auto/t_original/static/ioc/flag/3x2/ALB', '1972-08-26','https://upload.wikimedia.org/wikipedia/commons/5/52/Hymni_i_Flamurit_instrumental.ogg')
-    ('Alemania', 'https://gstatic.olympics.com/s1/f_auto/t_original/static/ioc/flag/3x2/GER', '1928-05-27','https://upload.wikimedia.org/wikipedia/commons/d/d0/Deutschlandlied_played_by_USAREUR_Band.ogg')
+    ('Albania', 'https://gstatic.olympics.com/s1/f_auto/t_original/static/ioc/flag/3x2/ALB', '1972-08-26','https://upload.wikimedia.org/wikipedia/commons/5/52/Hymni_i_Flamurit_instrumental.ogg'),
+    ('Alemania', 'https://gstatic.olympics.com/s1/f_auto/t_original/static/ioc/flag/3x2/GER', '1928-05-27','https://upload.wikimedia.org/wikipedia/commons/d/d0/Deutschlandlied_played_by_USAREUR_Band.ogg'),
     ('Argentina', 'https://gstatic.olympics.com/s1/f_auto/t_original/static/ioc/flag/3x2/ARG', '1924-07-05','https://upload.wikimedia.org/wikipedia/commons/e/ef/United_States_Navy_Band_-_Himno_Nacional_Argentino.ogg'),
     ('China', 'https://gstatic.olympics.com/s1/f_auto/t_original/static/ioc/flag/3x2/CHN', '1924-07-05','https://upload.wikimedia.org/wikipedia/commons/b/be/National_anthem_of_the_Republic_of_China_%28Taiwan%29_%E4%B8%AD%E8%8F%AF%E6%B0%91%E5%9C%8B%E5%9C%8B%E6%AD%8C%28%E6%BC%94%E5%A5%8F%E7%89%88%29.ogg'),
     ('Estados Unidos', 'https://gstatic.olympics.com/s1/f_auto/t_original/static/ioc/flag/3x2/USA', '1896-04-06','https://upload.wikimedia.org/wikipedia/commons/6/65/Star_Spangled_Banner_instrumental.ogg'),
