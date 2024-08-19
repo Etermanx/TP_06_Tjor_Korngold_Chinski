aaac4acef131eefb7b5896122b71d8418acfa738
Lucas Tjor <tjor75@gmail.com>
2024-08-19 18:51:09 -0300

    Arreglar CrearPaises.sql y crear CrearDeportistas.sql

---
+++ sql/CrearDeportistas.sql
@@ -0,0 +1,9 @@
+USE JJOO;
+
+INSERT INTO Deportistas (Apellido, Nombre, FechaNacimiento, Foto, IdPais)
+VALUES
+    ('Fernández', 'Carlos',  '1985-03-15', 'https://static.vecteezy.com/system/resources/previews/001/131/187/large_2x/serious-man-portrait-real-people-high-definition-grey-background-photo.jpg', 1),
+    ('Pérez', 'Alejandra', '1990-07-22' 'https://www.freejpg.com.ar/image-900/5d/5d4f/F100012509-rostro_de_una_mujer_joven_latina_mirando_a_la_camara_y_sonriendo.jpg', 6),
+    ('Rodríguez', 'Franco', '1992-03-05', 'https://c.pxhere.com/photos/e8/60/smile_profile_face_male_portrait_young_person_glasses-451653.jpg!d', 4),
+    ('Garcia', 'Alicia', '2001-11-11', 'https://ricamente.net/wp-content/uploads/2019/11/Mujer-pensando.jpg', 7),
+    ('Hernández', 'Pedro', '24-12-1999', 'http://2.bp.blogspot.com/-D2QYZk2qnIc/T3KtJOepeRI/AAAAAAAAAAU/fxbgUXABQkI/s1600/CHINO_20twitter.jpg', 5);
