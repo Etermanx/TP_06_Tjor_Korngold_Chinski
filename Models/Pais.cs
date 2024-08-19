e7e24742f7c7e9980bcce555e13858e8f8b81f53
Lucas Tjor <tjor75@gmail.com>
2024-08-19 09:19:39 -0300

    Hacer Himnos en el DetallePais

--- Models/Pais.cs
+++ Models/Pais.cs
@@ -3,4 +3,5 @@ public class Pais{
     public string Nombre { get; set; }
     public string Bandera { get; set; }
     public DateTime FechaFundacion { get; set; }
+    public string Himno {get;set;}
 }
